cfd0aebc44deebc35f8a3f4197e32d1f1307d7c7
yakiramar <33864769+yakiramar@users.noreply.github.com>
2018-04-22 16:30:52 +0300

    Update CircularInt.cpp

--- CircularInt.cpp
+++ CircularInt.cpp
@@ -17,11 +17,20 @@ CircularInt& CircularInt:: operator+=(int num) {
     cur++;
      return *this;
  }
-//   CircularInt::CircularInt& operator++(int) { 
-//       this->cur++;
-//         return *this;
-//     }
+ CircularInt CircularInt:: operator+(const CircularInt& c){
+     return CircularInt{(cur+c.cur)%e,e};
+ }
+ 
+ CircularInt& CircularInt:: operator *=(int num){
+     cur=(cur*num)%e;
+     return *this;
+ }
       const CircularInt CircularInt:: operator-()  {
         return CircularInt{e-cur,e};
     }
+  CircularInt CircularInt:: operator/(int num){
+     if(cur%num!=0){throw string("eror"); }
+     return CircularInt{cur/num,e};
+ }
+
     
