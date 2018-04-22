5eab33a23b3fc0fd4d445d330af108be57b2bfc3
yakiramar <33864769+yakiramar@users.noreply.github.com>
2018-04-22 16:29:56 +0300

    Update CircularInt.hpp

--- CircularInt.hpp
+++ CircularInt.hpp
@@ -7,11 +7,17 @@ class CircularInt {
        int cur;
        CircularInt(int s,int e);
        CircularInt& operator+=(int num);
-       friend ostream& operator<< (ostream& os, const CircularInt& c);
        CircularInt& operator++(int) ;
+       CircularInt operator+(const CircularInt& c);//hour+hour
        const CircularInt operator-();
+       CircularInt& operator *=(int num);
+       CircularInt operator/(int num);
+       
+       //friend function
+       friend ostream& operator<< (ostream& os, const CircularInt& c);
        friend CircularInt operator-(const int& num, CircularInt& c);
        
+    //   friend CircularInt operator+(const CircularInt& c1, const CircularInt& c2);
       
      
 
@@ -28,6 +34,9 @@ class CircularInt {
    c.cur=((num+12)-c.cur)%c.e;
    return c;
    }
+//   inline friend CircularInt operator+(const CircularInt& c1, const CircularInt& c2){//hour+hour
+//     return CircularInt{(c1.cur+c2.cur)%c1.e,c1.e}
+//   }
 
 
 
