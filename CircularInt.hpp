8156690ab24aa723990a61d76c5466297d0c91eb
yakiramar <33864769+yakiramar@users.noreply.github.com>
2018-04-26 18:16:28 +0300

    Update CircularInt.hpp

--- CircularInt.hpp
+++ CircularInt.hpp
@@ -6,18 +6,50 @@ class CircularInt {
     public:
        int cur;
        CircularInt(int s,int e);
-       CircularInt& operator+=(int num);
-       CircularInt& operator++(int) ;
+       
+       CircularInt& operator++(int) ;//++postfix
+       CircularInt& operator++();//++perfix
+       CircularInt& operator--(int);//--postfix
+       CircularInt& operator--();//--perfix
+       
+       CircularInt operator*( const CircularInt& c);//a*b
        CircularInt operator+(const CircularInt& c);//hour+hour
-       const CircularInt operator-();
-       CircularInt& operator *=(int num);
-       CircularInt operator/(int num);
+       CircularInt operator%(const CircularInt& c);
+       const CircularInt operator-();//e-cur
+       CircularInt operator/(int num);//cur/number
+       CircularInt operator /(const CircularInt& c);//a/b
+       CircularInt& operator=(const CircularInt& c);//a=b
+       
+       
+       CircularInt& operator *=(int num);//(number*cur)%e
+       CircularInt& operator+=(int num);//cur+number
+       CircularInt& operator-=(int num);//cur-number
+       
+       CircularInt& operator-=(const CircularInt& c);//this cur-c.cur
+       CircularInt& operator+=(const CircularInt& c);//this cur+c.cur
+       CircularInt& operator*=(const CircularInt& c);//this cur*c.cur
+       CircularInt& operator/=(const CircularInt& c);//this cur/c.cur
+       
+       
        
        //friend function
-       friend ostream& operator<< (ostream& os, const CircularInt& c);
+       friend ostream& operator<< (ostream& os, const CircularInt& c);//output
        friend CircularInt operator-(const int& num, CircularInt& c);
+       friend bool operator==(const CircularInt& c1, const CircularInt& c2);//a=b?
+       friend bool operator!=(const CircularInt& c1, const CircularInt& c2);//a!=b?
+       friend bool operator>(const CircularInt& c1, const CircularInt& c2);//a>b?
+       friend bool operator<(const CircularInt& c1, const CircularInt& c2);//a<b
+       friend bool operator>=(const CircularInt& c1, const CircularInt& c2);//a>=b
+       friend bool operator<=(const CircularInt& c1, const CircularInt& c2);//a<=b
+       friend bool operator!(const CircularInt& c1);//!a
+       friend bool operator&&(const CircularInt& c1, const CircularInt& c2);//a&&b
+       friend bool operator||(const CircularInt& c1, const CircularInt& c2);//a||b
        
-    //   friend CircularInt operator+(const CircularInt& c1, const CircularInt& c2);
+
+       
+
+       
+   
       
      
 
@@ -30,13 +62,50 @@ class CircularInt {
     os << c.cur << endl;
     return os;
     }
+    
    inline CircularInt operator-(const int& num, CircularInt& c){// int-hour
    c.cur=((num+12)-c.cur)%c.e;
    return c;
    }
-//   inline friend CircularInt operator+(const CircularInt& c1, const CircularInt& c2){//hour+hour
-//     return CircularInt{(c1.cur+c2.cur)%c1.e,c1.e}
-//   }
+   
+   inline bool operator==(const CircularInt& c1, const CircularInt& c2) {
+    
+    return ((c1.s==c2.s)&&(c1.e==c2.e)&&(c1.cur==c2.cur));
+   }
+   
+  inline bool operator!=(const CircularInt& c1, const CircularInt& c2){
+      return((c1.s!=c2.s)||(c1.e!=c2.e)||(c1.cur!=c2.cur));
+   }
+  
+  inline bool operator>(const CircularInt& c1, const CircularInt& c2){
+      return(c1.cur>c2.cur);
+  }
+  
+  inline bool operator<(const CircularInt& c1, const CircularInt& c2){
+      return(c1.cur<c2.cur);
+  }
+  
+  inline bool operator>=(const CircularInt& c1, const CircularInt& c2){
+      return(c1.cur>=c2.cur);
+  }
+  
+  inline bool operator<=(const CircularInt& c1, const CircularInt& c2){
+      return(c1.cur<=c2.cur);
+  }
+  
+  inline bool operator!(const CircularInt& c1){
+      return(!c1.cur);
+  }
+  
+  inline bool operator&&(const CircularInt& c1, const CircularInt& c2){
+      return(c1.cur&&c2.cur);
+  }
+  
+  inline bool operator||(const CircularInt& c1, const CircularInt& c2){
+      return(c1.cur||c2.cur);
+  }
+   
+
 
 
 
