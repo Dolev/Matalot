31e05b59b171b43b1d7166af6faa3d8d874dfe78
yakiramar <33864769+yakiramar@users.noreply.github.com>
2018-04-27 14:58:44 +0300

    Update CircularInt.hpp

--- CircularInt.hpp
+++ CircularInt.hpp
@@ -1,6 +1,5 @@
 #include<iostream>
 using namespace std;
-#include<string>
 class CircularInt {
     private:
        int s,e;
@@ -51,9 +50,9 @@ class CircularInt {
        
        
        friend CircularInt operator-(const int& num, CircularInt& c);
-       friend CircularInt operator+ (const int& num, CircularInt& c); 
-       friend CircularInt operator* (const int& num, CircularInt& c);
-       friend CircularInt operator/ (const int& num, CircularInt& c);
+    //   friend CircularInt operator+ (const int& num, CircularInt& c); 
+    //   friend CircularInt operator* (const int& num, CircularInt& c);
+    //   friend CircularInt operator/ (const int& num, CircularInt& c);
        
        
        friend bool operator==(const CircularInt& c1, const CircularInt& c2);//a=b?
@@ -115,23 +114,29 @@ class CircularInt {
    c.fix();
    return c;
    }
-   inline CircularInt operator+ (const int& num, CircularInt& c){
-    c.cur+=num;
-    c.fix();
-    return c;
-   }
-   inline CircularInt operator* (const int& num, CircularInt& c){
-    c.cur*=num;
-    c.fix();
-    return c;
-   }
-   inline CircularInt operator/ (const int& num, CircularInt& c){
-    if(c.cur==0){throw string(" eror:we can't divide in zero!");}
-    if(num%c.cur!=0){throw string(" eror");}
-    c.cur=num/c.cur;
-    c.fix()
-    return c;
-   }
+//   inline CircularInt operator-(const int& num, CircularInt& c){// int-hour
+//   c.cur=num-c.cur;
+//   c.fix();
+//   return c;
+//   }
+//   inline CircularInt operator+ (const int& num, CircularInt& c){
+//     c.cur+=num;
+//     c.fix();
+//     return c;
+//   }
+//   inline CircularInt operator* (const int& num, CircularInt& c){
+//     c.cur*=num;
+//     c.fix();
+//     return c;
+//   }
+//   inline CircularInt operator/ (const int& num, CircularInt& c){
+//     if(c.cur==0){throw string(" eror:we can't divide in zero!");}
+//     if(num%c.cur!=0){throw string(" eror");}
+//     c.cur=num/c.cur;
+//     c.fix();
+//     return c;
+//   }
+   
    
    
    
