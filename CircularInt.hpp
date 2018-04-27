dca4d9bf51d897f704ecbeb56892736c15c66230
yakiramar <33864769+yakiramar@users.noreply.github.com>
2018-04-27 12:48:15 +0300

    Update CircularInt.hpp

--- CircularInt.hpp
+++ CircularInt.hpp
@@ -3,27 +3,35 @@ using namespace std;
 class CircularInt {
     private:
        int s,e;
+       void fix();
     public:
        int cur;
        CircularInt(int s,int e);
        
-       CircularInt& operator++(int) ;//++postfix
+       const CircularInt operator++(int) ;//++postfix
        CircularInt& operator++();//++perfix
-       CircularInt& operator--(int);//--postfix
+       const CircularInt operator--(int);//--postfix
        CircularInt& operator--();//--perfix
        
        CircularInt operator*( const CircularInt& c);//a*b
        CircularInt operator+(const CircularInt& c);//hour+hour
        CircularInt operator%(const CircularInt& c);
        const CircularInt operator-();//e-cur
-       CircularInt operator/(int num);//cur/number
        CircularInt operator /(const CircularInt& c);//a/b
+       CircularInt operator-(const CircularInt& c);
        CircularInt& operator=(const CircularInt& c);//a=b
+       CircularInt& operator=(int num);//a=num;
        
+       const CircularInt operator+(int num);//cur+num
+       const CircularInt operator/(int num);//cur/number
+       const CircularInt operator-(int num);//cur/number
+       const CircularInt operator*(int num);//cur/number
        
        CircularInt& operator *=(int num);//(number*cur)%e
        CircularInt& operator+=(int num);//cur+number
        CircularInt& operator-=(int num);//cur-number
+       CircularInt& operator/=(int num);//cur/number
+       
        
        CircularInt& operator-=(const CircularInt& c);//this cur-c.cur
        CircularInt& operator+=(const CircularInt& c);//this cur+c.cur
@@ -34,6 +42,9 @@ class CircularInt {
        
        //friend function
        friend ostream& operator<< (ostream& os, const CircularInt& c);//output
+       friend istream& operator>> (istream& input,  CircularInt& c);  // (cin)
+       
+       
        friend CircularInt operator-(const int& num, CircularInt& c);
        friend bool operator==(const CircularInt& c1, const CircularInt& c2);//a=b?
        friend bool operator!=(const CircularInt& c1, const CircularInt& c2);//a!=b?
@@ -61,6 +72,17 @@ class CircularInt {
    inline ostream& operator<< (ostream& os, const CircularInt& c) {
     os << c.cur << endl;
     return os;
+    }
+    inline istream& operator>> (istream& input,  CircularInt& c){ // (cin)
+    int x;
+        input >> x ;
+        if(x<c.s || x>c.e){
+    throw string("The number "+to_string(x)+" is not in the range )");
+        }
+        else{
+        c.cur=x;           
+        }
+        return input;
     }
     
    inline CircularInt operator-(const int& num, CircularInt& c){// int-hour
