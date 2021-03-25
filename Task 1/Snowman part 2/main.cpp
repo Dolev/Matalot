5bfb6d7b9a84f8e99a73e83775d61bd878c657e0
Dolev .H <33119992+Dolev@users.noreply.github.com>
2021-03-25 20:25:15 +0200

    Add files via upload

---
+++ Task 1/Snowman part 2/main.cpp
@@ -0,0 +1,16 @@
+// my first program in C++
+#include <iostream>
+#include "snowman.cpp"
+
+int main()
+{
+    
+    std::cout << ariel::snowman(41312321) << std::endl;
+    std::cout << ariel::snowman(31214411) << std::endl;
+    std::cout << ariel::snowman(23114411) << std::endl;
+    //std::cout << ariel::snowman(41143211) << std::endl;
+    //std::cout<< s;
+
+  //CHECK(nospaces(snowman(11114411)) == nospaces(" _===_ \n (.,.) \n ( : ) \n ( : )"));
+}
+
